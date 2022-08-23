3055363082e4aaf61f427d6df93fd87d4457df31
Eric Qiu <sololaneonly1@gmail.com>
2022-08-23 00:42:35 -0400

    day 3 learning progress

---
+++ Day-3/Readme.md
@@ -0,0 +1,3 @@
+Learned about objects and arrays, how they function, and the rules of typescript has applied on them.
+
+Day 4 will be learning explicit types.
---
+++ Day-3/sandbox.ts
@@ -0,0 +1,30 @@
+//arrays
+let names = ["luigi", "mario"];
+names.push("toad");
+names.push("3");
+names[0] = "false";
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+//numbers.push('shaun');
+
+let mixed = ["ken", 4, "chun-li", 8, 9];
+
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+
+//objects
+let ninja = {
+  name: "mario",
+  belt: "black",
+  age: 30,
+};
+
+ninja.age = 40;
+ninja.name = "ryu";
+//ninja.age = '20'
+//ninja.skills = ["fighting", "sneaking"];
+
+//cannot override the object to be another array however can be overwrited as a new object. The new object has to match the old object, values can be the same but has to have same properties.
